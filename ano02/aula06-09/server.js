60f69eb8dd378c69bbaaa91d9fa7e6d0e7ca99ce
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-05-30 22:45:19 -0300

    Modificando pastas

---
+++ ano02/aula06-09/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const {Sequelize} = require("sequelize");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+//Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
+
+// Define the Product model
+const { DataTypes } = require("sequelize");
+
+
+app.listen(port, () => console.log(`rodando no link ${URL}`));
---
+++ ano02/aula06-09/serverOld.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+const URL = `http://127.0.0.1:${port}`;
+
+app.use(express.json());
+
+const produtos = [
+  {
+    id: 1,
+    nome: "Pregos",
+    preco: 5.99
+  },
+  {
+    id: 2,
+    nome: "Martelo",
+    preco: 12.99
+  },
+  {
+    id: 3,
+    nome: "Trena",
+    preco: 7.80
+  },
+]
+
+app.get("/produtos", (req, res) =>{
+  res.json(produtos);
+});
+
+app.get("/produtos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const produto = produtos.find(el => el.id === id);
+
+  if (produto) {
+    res.json(produto);
+  } else {
+    res.status(404).json({
+      erro: "Produto não encontrado"
+    });
+  }
+});
+
+app.post("/produtos", (req, res) => {
+  const { nome, preco } = req.body;
+  const id = produtos.length + 1;
+  const novoProduto = { id, nome, preco };
+  produtos.push(novoProduto);
+  res.status(201).json(novoProduto);
+});
+
+app.put("/produtos/:id", async (req, res) => {
+  const id = Number(req.params.id);
+  const {nome, preco} = req.body;
+  const index = produtos.findIndex(el => el.id === id);
+
+  if (index !== -1) {
+    produtos[index] = { ...produtos[index], nome, preco };
+    res.json(produtos[index]);
+  } else {
+    res.status(404).json({ error: "Product not found" });
+  }
+});
+
+app.delete("/produtos/:id", (req, res) => {
+  let id = Number(req.params.id);
+  produtos = produtos.filter(el => el.id !== id);
+  res.sendStatus(204);
+});
+
+app.listen(port, () => console.log(`rodando no link ${URL}`));
