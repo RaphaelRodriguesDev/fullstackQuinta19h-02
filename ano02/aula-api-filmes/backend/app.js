4623ac575649949b3638304dd8880545674c9e0b
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-08-21 18:51:49 -0300

    Corrigindo Erros

--- ano02/aula-api-filmes/backend/app.js
+++ ano02/aula-api-filmes/backend/app.js
@@ -2,5 +2,5 @@ const app = require("./src/config/server");
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`Rodando no link => https://localhost:${port}`);
+  console.log(`Rodando no link => http://localhost:${port}`);
 });
